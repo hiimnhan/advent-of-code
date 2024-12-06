27f668827eb57f95201ea0f056d19a61b505ff20
nhan.nguyen <nhan.nguyen@innogram.co>
2024-12-06 17:50:27 +0700

    run ~ 25s

--- solutions/2024/day_06/solution.py
+++ solutions/2024/day_06/solution.py
@@ -4,6 +4,7 @@
 
 from ...base import StrSplitSolution, answer
 import sys
+import copy
 
 sys.setrecursionlimit(10000)
 
@@ -18,64 +19,70 @@ class Solution(StrSplitSolution):
         "v": "<",
         "<": "^",
     }
+    def print_grid(self, grid):
+        for row in grid:
+            print("".join(row))
+        print()
 
-    # traverse the grid when hit # turn right, count the step
-    def traverse(self, grid, r, c, visited):
+    # traverse the grid when hit # turn right, count, loop the step
+    def traverse(self, grid, r, c, visited, visited_cycle) -> tuple[set, bool]:
         def out_of_bound(r, c):
             return r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0])
 
-        def print_grid():
-            for row in grid:
-                print("".join(row))
-            print()
 
-        print(r, c, grid[r][c])
+        is_cycle = False
+        if (r, c, grid[r][c]) in visited_cycle:
+            is_cycle = True
+            return visited, is_cycle
+        visited_cycle.add((r, c, grid[r][c]))
         match grid[r][c]:
             case "^":
                 visited.add((r, c))
                 if out_of_bound(r - 1, c):
-                    print_grid()
-                    return len(visited)
+                    # print_grid()
+                    return visited, is_cycle
                 if grid[r - 1][c] != "#":
                     grid[r - 1][c] = grid[r][c]
-                    self.traverse(grid, r - 1, c, visited)
+                    is_cycle = self.traverse(grid, r - 1, c, visited, visited_cycle)[1]
                 else:
                     grid[r][c] = self.mp[grid[r][c]]
-                    self.traverse(grid, r, c, visited)
+                    is_cycle = self.traverse(grid, r, c, visited, visited_cycle)[1]
             case ">":
                 visited.add((r, c))
                 if out_of_bound(r, c + 1):
-                    print_grid()
-                    return len(visited)
+                    # print_grid()
+                    return visited, is_cycle
                 if grid[r][c + 1] != "#":
                     grid[r][c + 1] = grid[r][c]
-                    self.traverse(grid, r, c + 1, visited)
+                    is_cycle = self.traverse(grid, r, c + 1, visited, visited_cycle)[1]
                 else:
                     grid[r][c] = self.mp[grid[r][c]]
-                    self.traverse(grid, r, c, visited)
+                    is_cycle = self.traverse(grid, r, c, visited, visited_cycle)[1]
             case "v":
                 visited.add((r, c))
                 if out_of_bound(r + 1, c):
-                    print_grid()
-                    return len(visited)
+                    # print_grid()
+                    return visited, is_cycle
                 if grid[r + 1][c] != "#":
                     grid[r + 1][c] = grid[r][c]
-                    self.traverse(grid, r + 1, c, visited)
+                    is_cycle = self.traverse(grid, r + 1, c, visited, visited_cycle)[1]
                 else:
                     grid[r][c] = self.mp[grid[r][c]]
-                    self.traverse(grid, r, c, visited)
+                    is_cycle = self.traverse(grid, r, c, visited, visited_cycle)[1]
             case "<":
                 visited.add((r, c))
                 if out_of_bound(r, c - 1):
-                    print_grid()
-                    return len(visited)
+                    # print_grid()
+                    return visited, is_cycle
                 if grid[r][c - 1] != "#":
                     grid[r][c - 1] = grid[r][c]
-                    self.traverse(grid, r, c - 1, visited)
+                    is_cycle = self.traverse(grid, r, c - 1, visited, visited_cycle)[1]
                 else:
                     grid[r][c] = self.mp[grid[r][c]]
-                    self.traverse(grid, r, c, visited)
-        return len(visited)
+                    is_cycle = self.traverse(grid, r, c, visited, visited_cycle)[1]
+        if is_cycle:
+            return visited, is_cycle
+        return visited, is_cycle
 
     @answer(4696)
     def part_1(self) -> int:
@@ -85,16 +92,35 @@ class Solution(StrSplitSolution):
         ]
         R = len(grid)
         C = len(grid[0])
-        print(grid)
 
-        visited = set()
 
         for row in range(R):
             for col in range(C):
                 if grid[row][col] == "^":
-                    return self.traverse(grid, row, col, visited)
+                    l, _ = self.traverse(grid, row, col, set(), set())
+                    return len(l)
         return 0
 
     # @answer(1234)
     def part_2(self) -> int:
-        pass
+        grid = [
+            [self.input[i][j] for j in range(len(self.input[0]))]
+            for i in range(len(self.input))
+        ]
+        R = len(grid)
+        C = len(grid[0])
+        ans = set()
+
+        for row in range(R):
+            for col in range(C):
+                if grid[row][col] == "^":
+                    _grid = copy.deepcopy(grid)
+                    visited, _ = self.traverse(_grid, row, col, set(), set())
+                    for r, c in visited:
+                        changed_grid = copy.deepcopy(grid) 
+                        changed_grid[r][c] = "#"
+                        _, is_cycle = self.traverse(changed_grid, row, col, set(), set())
+                        if is_cycle:
+                            ans.add((r, c)) 
+
+        return len(ans)
