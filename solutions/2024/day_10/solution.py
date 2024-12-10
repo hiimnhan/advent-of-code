6cf0d1ff2e38f7a48d373404adce755db39157d6
nhan.nguyen <nhan.nguyen@innogram.co>
2024-12-10 13:10:48 +0700

    2024 - day 10

---
+++ solutions/2024/day_10/solution.py
@@ -0,0 +1,61 @@
+# Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
+
+# puzzle prompt: https://adventofcode.com/2024/day/10
+
+from collections import deque
+from solutions.utils.util import is_out_of_bounds, make_grid
+from ...base import StrSplitSolution, answer
+import sys
+
+sys.setrecursionlimit(10000)
+
+
+class Solution(StrSplitSolution):
+    _year = 2024
+    _day = 10
+
+    def traverse(self, grid, x, y) -> tuple[int, int]:
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        # BFS
+        queue = deque([(x, y)])
+        visited = set()
+
+        reachable = []
+
+        while queue:
+            r, c = queue.popleft()
+            val = grid[r][c]
+
+            for dx, dy in directions:
+                nx, ny = r + dx, c + dy
+
+                if not is_out_of_bounds(grid, nx, ny):
+                    next_val = grid[nx][ny]
+                    if next_val == val + 1 and (nx, ny) not in visited:
+                        queue.append((nx, ny))
+                        if next_val == 9:
+                            reachable.append((nx, ny))
+
+        return len(set(reachable)), len(reachable)
+
+    @answer(638)
+    def part_1(self) -> int:
+        ans = 0
+        grid = make_grid(self.input, to_int=True)
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 0:
+                    reachable, _ = self.traverse(grid, r, c)
+                    ans += reachable
+        return ans
+
+    @answer(1289)
+    def part_2(self) -> int:
+        ans = 0
+        grid = make_grid(self.input, to_int=True)
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 0:
+                    _, reachable = self.traverse(grid, r, c)
+                    ans += reachable
+        return ans
--- solutions/utils/util.py
+++ solutions/utils/util.py
@@ -4,5 +4,10 @@ def print_grid(grid: list[list[str]]):
     print()
 
 
-def make_grid(input: list[str]):
+def make_grid(input: list[str], to_int=False):
+    if to_int:
+        return [[int(cell) for cell in row] for row in input]
     return [list(row) for row in input]
+
+def is_out_of_bounds(grid, r, c):
+    return r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0])
