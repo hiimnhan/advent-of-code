02c7e226b2319814f26f3f386468216c42f181dc
nhanncv <vinhnhan1999bc@gmail.com>
2024-12-12 18:35:00 +0700

    stuck day 12 - p2

---
+++ solutions/2024/day_12/solution.py
@@ -0,0 +1,36 @@
+# Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
+
+# puzzle prompt: https://adventofcode.com/2024/day/12
+
+from solutions.utils.grid import connected_regions, make_grid
+from ...base import StrSplitSolution, answer
+import sys
+
+sys.setrecursionlimit(10000)
+
+
+class Solution(StrSplitSolution):
+    _year = 2024
+    _day = 12
+
+    # @answer(1234)
+    def part_1(self) -> int:
+        ans = 0
+        grid = make_grid(self.input)
+        components = connected_regions(grid)
+        # print(components)
+        for type, regions in components.items():
+            for region in regions:
+                ans += len(region[0]) * region[1]
+        return ans
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        ans = 0
+        grid = make_grid(self.input)
+        components = connected_regions(grid)
+        for type, regions in components.items():
+            for region in regions:
+                print(type, len(region[0]), region[2])
+                ans += len(region[0]) * region[2]
+        return ans
--- solutions/utils/grid.py
+++ solutions/utils/grid.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def print_grid(grid: list[list[str]]):
     for row in grid:
         print("".join(row))
@@ -12,6 +15,71 @@ def make_grid(input: list[str], to_int=False):
 
 def is_out_of_bounds(grid, r, c):
     return r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0])
+
+
+def next_coord(r, c, dr, dc):
+    return r + dr, c + dc
+
+
+def connected_regions(
+    grid,
+) -> dict[str, list[tuple[set[tuple[int, int]], int, int]]]:
+    R, C = len(grid), len(grid[0])
+    visited = [[False] * C for _ in range(R)]
+    regions = {}
+
+    def dfs(r, c, type):
+        if is_out_of_bounds(grid, r, c) or visited[r][c] or grid[r][c] != type:
+            return set(), set()
+        visited[r][c] = True
+        region_plots = {(r, c)}
+        perimeter = set()
+
+        directions = [RIGHT, DOWN, LEFT, UP]
+        for dr, dc in directions:
+            nr, nc = r + dr, c + dc
+
+            if is_out_of_bounds(grid, nr, nc) or grid[nr][nc] != type:
+                perimeter.add((r, c, dr, dc))
+                continue
+
+            if not visited[nr][nc]:
+                new_plots, new_perimeter = dfs(nr, nc, type)
+                region_plots.update(new_plots)
+                perimeter.update(new_perimeter)
+        return region_plots, perimeter
+
+    for r in range(R):
+        for c in range(C):
+            if visited[r][c]:
+                continue
+            type = grid[r][c]
+            print("----")
+            print(type)
+            plots, perimeter = dfs(r, c, type)
+            if plots:
+                corners = 0
+                for x, y in plots:
+                    top = next_coord(x, y, *UP)
+                    left = next_coord(x, y, *LEFT)
+                    right = next_coord(x, y, *RIGHT)
+                    bottom = next_coord(x, y, *DOWN)
+
+                    tr = top not in plots and right not in plots
+                    tl = top not in plots and left not in plots
+                    br = bottom not in plots and right not in plots
+                    bl = bottom not in plots and left not in plots
+
+                    print(
+                        f"x: {x}, y: {y}, corners: {tl, tr, bl, br} -> {tl + tr + bl + br}"
+                    )
+
+                    corners += tl + tr + bl + br
+
+                if type not in regions:
+                    regions[type] = []
+                regions[type].append((plots, len(perimeter), corners))
+    return regions
 
 
 UP = (-1, 0)
