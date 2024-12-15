8d0feea6cef37e372934c6868329d87406a1bb7a
nhanncv <vinhnhan1999bc@gmail.com>
2024-12-15 16:21:40 +0700

    day 14 + 15 - part 1

---
+++ solutions/2024/day_14/solution.py
@@ -0,0 +1,65 @@
+# Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
+
+# puzzle prompt: https://adventofcode.com/2024/day/14
+
+from ...base import StrSplitSolution, answer
+import sys
+from collections import defaultdict
+
+sys.setrecursionlimit(10000)
+
+
+class Solution(StrSplitSolution):
+    _year = 2024
+    _day = 14
+
+    WIDTH = 101
+    HEIGHT = 103
+
+    def parse(self, input):
+        data = []
+        for line in input:
+            p, v = line.split(" ")
+            px, py = map(int, p.split("=")[1].split(","))
+            vx, vy = map(int, v.split("=")[1].split(","))
+            data.append((px, py, vx, vy))
+        return data
+
+    def check_is_center(self, r, c):
+        hw = self.WIDTH // 2
+        hh = self.HEIGHT // 2
+
+        return r == hh and c == hw
+
+    def process(self, px, py, vx, vy, time):
+        dx = vx * time
+        dy = vy * time
+
+        return ((px + dx) % self.WIDTH, (py + dy) % self.HEIGHT)
+
+    # @answer(1234)
+    def part_1(self) -> int:
+        ans = 1
+        data = self.parse(self.input)
+        q1, q2, q3, q4 = 0, 0, 0, 0
+
+        for d in data:
+            px, py, vx, vy = d
+            x, y = self.process(px, py, vx, vy, 100)
+            print(x, y)
+            if x < self.WIDTH // 2 and y < self.HEIGHT // 2:
+                q1 += 1
+            elif x < self.WIDTH // 2 and y > self.HEIGHT // 2:
+                q2 += 1
+            elif x > self.WIDTH // 2 and y < self.HEIGHT // 2:
+                q3 += 1
+            elif x > self.WIDTH // 2 and y > self.HEIGHT // 2:
+                q4 += 1
+
+        print(q1, q2, q3, q4)
+        return q1 * q2 * q3 * q4
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        ans = 0
+        return ans
---
+++ solutions/2024/day_15/solution.py
@@ -0,0 +1,82 @@
+# Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
+
+# puzzle prompt: https://adventofcode.com/2024/day/15
+
+from ...base import StrSplitSolution, answer
+import sys
+from solutions.utils.grid import (
+    DOWN,
+    LEFT,
+    RIGHT,
+    UP,
+    is_out_of_bounds,
+    make_grid,
+    next_coord,
+)
+
+sys.setrecursionlimit(10000)
+
+
+class Solution(StrSplitSolution):
+    _year = 2024
+    _day = 15
+
+    separator = "\n\n"
+
+    directions = {
+        ">": RIGHT,
+        "<": LEFT,
+        "^": UP,
+        "v": DOWN,
+    }
+
+    def push(self, grid, r, c, step, walls, boxes):
+        d = self.directions[step]
+        nxt = next_coord(r, c, *d)
+        if nxt in walls:
+            return False
+        if nxt in boxes:
+            if not self.push(grid, *nxt, step, walls, boxes):
+                return False
+        boxes.remove((r, c))
+        boxes.add(nxt)
+        return True
+
+    # @answer(1234)
+    def part_1(self) -> int:
+        ans = 0
+        grid = self.input[0].split("\n")
+        steps = self.input[1]
+        grid = make_grid(grid)
+        R, C = len(grid), len(grid[0])
+        walls = set()
+        boxes = set()
+
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c] == "@":
+                    robot = (r, c)
+                elif grid[r][c] == "#":
+                    walls.add((r, c))
+                elif grid[r][c] == "O":
+                    boxes.add((r, c))
+        for step in steps:
+            d = self.directions[step]
+            nxt = next_coord(*robot, *d)
+            if nxt in walls:
+                continue
+            if nxt in boxes:
+                if not self.push(grid, nxt[0], nxt[1], step, walls, boxes):
+                    continue
+            assert nxt not in boxes
+            robot = nxt
+
+        for box in boxes:
+            ans += box[0] * 100 + box[1]
+
+        return ans
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        ans = 0
+        return ans
